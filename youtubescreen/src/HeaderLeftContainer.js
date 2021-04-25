44b9135a6ef436d6d0ddc176b3e9e5b61fa9901b
Mehru <mehrunnisa533@gmail.com>
2021-04-25 22:39:37 +0500

    Header improved

--- youtubescreen/src/HeaderLeftContainer.js
+++ youtubescreen/src/HeaderLeftContainer.js
@@ -1,13 +1,33 @@
 import React from "react";
 import "./HeaderLeftContainer.css";
 import { FaReply } from "react-icons/fa";
+import { BiSearch } from "react-icons/bi";
+import { BiMinus } from "react-icons/bi";
+import { BiDuplicate } from "react-icons/bi";
+import { BiX } from "react-icons/bi";
+
 function HeaderLeftContainer() {
   return (
     <div className="HeaderLeftContainer">
       <div className="lefticonVisual">
         <FaReply />
       </div>
-      <p>StartPage-Microsoft Visual Studio</p>
+      <div id="visualtextheading">
+        <p>StartPage-Microsoft Visual Studio</p>
+      </div>
+      <div className="containersearchbox">
+        <input
+          className="bg-white"
+          type="Search Bar"
+          placeholder="Quick Launch(Ctrl+Q)"
+        />
+      </div>
+      <div className="RightIconsofContainer">
+        <BiSearch />
+        <BiMinus />
+        <BiDuplicate />
+        <BiX />
+      </div>
     </div>
   );
 }
