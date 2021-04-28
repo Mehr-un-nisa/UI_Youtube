12065337de46bec47b5fe3a0bf261f3d65df672a
Mehru <mehrunnisa533@gmail.com>
2021-04-29 03:02:57 +0500

    Added: Button class added

--- youtubescreen/src/components/RightContainer.js
+++ youtubescreen/src/components/RightContainer.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { FaBluetoothB } from "react-icons/fa";
 import "./RightContainer.css";
 // import { FaReply } from "react-icons/fa";
 function RightContainer() {
   return (
-    <div className="bg-pink">
-      <button>All</button>
+    <div className="righticonheader">
+      <div className="buttonclass">
+        <button className="All"> All</button>
+        <button className="C"> C#</button>
+        <button className="databases"> Databases</button>
+        <button className="Cplus"> C++</button>
+        <button className="computers"> Computers</button>
+      </div>
     </div>
   );
 }
